3f3ae15c058c2f82a65563f2cdc22d0bf012bc22
Amna <amnabaara287@gmail.com>
2023-07-13 16:57:40 -0700

    My first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+char i;
+int b;
+long c;
+long long d;
+float e;
+printf("Size of a char: %zu byte(s)", sizeof(i));
+printf("Size of an int: %zu byte(s)", sizeof(b));
+printf("size of a long int: %zu byte(s)", sizeof(c));
+printf("Size of a long long int: %zu byte(s)", sizeof(d));
+printf("Size of a float: %zu byte(s)", sizeof(e));
+return (0);
+}
