01ade8b748b857f5b6a22a27b4392175a90e877e
Amna <amnabaara287@gmail.com>
2023-07-13 17:18:21 -0700

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,6 @@ printf("Size of an int: %zu byte(s)\n", sizeof(b));
 printf("size of a long int: %zu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 printf("Size of a float: %zu byte(s)\n", sizeof(e));
+fprintf(stderr, "Error: 405");
 return (0);
 }
