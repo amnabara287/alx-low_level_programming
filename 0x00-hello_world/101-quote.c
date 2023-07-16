8a5874164fec83142daafc34617590b619d602d6
Amna <amnabaara287@gmail.com>
2023-07-16 14:06:53 -0700

    My first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+char a[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, a, 59);
+return (1);
+}
