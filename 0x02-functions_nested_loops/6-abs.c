168a1892647d6643d0b87c6d6d954c80aa84799f
Amna <amnabaara287@gmail.com>
2023-07-18 16:16:09 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - utilizes on the _putchar funtion
+*@n : checks input
+*Return: return 1 or 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = (-1) * n;
+return (n);
+}
+}
