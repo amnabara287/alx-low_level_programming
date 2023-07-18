6dc3c454714e8982c8a8193389f5fbf3570f2016
Amna <amnabaara287@gmail.com>
2023-07-18 13:14:05 -0700

    My first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+* print_alphabet - utilizes on the _putchar funtion to print the alphapet a - z
 *main - Prints a random number and states whether
 *it is positive, negative, or zero.
 *Return: Always 0.
@@ -10,5 +11,4 @@ int ch;
 for (ch = 'a'; ch <= 'z' ; ch++)
 _putchar(ch);
 _putchar('\n');
-return (0);
 }
