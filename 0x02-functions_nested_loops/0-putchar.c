375340256f77061be206d913643b18fb7ae1fd4a
Amna <amnabaara287@gmail.com>
2023-07-18 11:24:19 -0700

    My first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Prints a random number and states whether
- * it is positive, negative, or zero.
+ *        it is positive, negative, or zero.
  *
  * Return: Always 0.
 */
-int main(void)
-{
-printf("_putchar\n");
+int main(void) {
+_putchar();
 return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ * it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+void _putchar(void)
+{
+printf("_putchar\n");
+}
