9c40224ac66bf0a91c02dc1d639dec794484459f
Amna <amnabaara287@gmail.com>
2023-07-18 15:57:41 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - utilizes on the _putchar funtion
+*@n: checks input
+*Return: return 1 or 0 or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('43');
+return (1);
+}
+if (n == 0)
+{
+_putchar('48');
+return (0);
+}
+if (n < 0)
+{
+_putchar('45');
+return (-1);
+}
+}
