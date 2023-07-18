4e290b2587fe8864665eadf3916131b7f948b97e
Amna <amnabaara287@gmail.com>
2023-07-18 13:06:19 -0700

    My first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * main - Prints a random number and states whether
- * it is positive, negative, or zero.
- * 
- * 
- * Return: Always 0.
- */
+*main - Prints a random number and states whether
+*it is positive, negative, or zero.
+*Return: Always 0.
+*/
 void print_alphabet(void)
 {
 int ch;
