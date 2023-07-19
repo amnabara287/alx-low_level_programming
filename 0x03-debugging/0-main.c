f3d5d2aadb0431f78a2d66470c69e100161e4353
Amna <amnabaara287@gmail.com>
2023-07-19 12:25:38 -0700

    My first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**                                                                                                                    
+* print_alphabet_x10 - utilizes on the _putchar funtion to                                                             
+* print the alphapet a - z 10 times                                                                                    
+*/
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("Number is positive");	     
+}
+else
+{
+printf("Number is negative");
+}
+return (0);
+}
