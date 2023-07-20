532f790318330802a015a02a24ee0000555d6d75
Amna <amnabaara287@gmail.com>
2023-07-20 05:17:59 -0700

    My first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,10 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-void positive_or_negative(int i)
+int main(void)
 {
 int i;
-i = 98
+i = 0
 positive_or_negative(i);
 return (0);
 }
