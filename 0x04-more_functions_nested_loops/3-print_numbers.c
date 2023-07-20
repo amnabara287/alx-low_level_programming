3b803732816ca1aee6915098c97f70ae08e65c9c
Amna <amnabaara287@gmail.com>
2023-07-20 11:21:21 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+char c;
+for (c = 48; c <= 57; i++)
+{
+_putchar (c);
+}
+_putchar('\n');
+}
