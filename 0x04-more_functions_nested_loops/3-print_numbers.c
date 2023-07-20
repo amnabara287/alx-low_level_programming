06748acff26b0fc06fe750d08fa732b5aa172355
Amna <amnabaara287@gmail.com>
2023-07-20 11:29:56 -0700

    My first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
-include "main.h"
+#include "main.h"
 
 /**
 *print_numbers - print 0123456789
-*
 *Return: void
 */
 
 void print_numbers(void)
 {
-char c;
-for (c = 48; c <= 57; c++)
-{
-_putchar (c);
-}
-_putchar('\n');
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
