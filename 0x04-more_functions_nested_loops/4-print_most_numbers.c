c775b8e92ab653cb0ed5fc1d3e9c6884ab279b0c
Amna <amnabaara287@gmail.com>
2023-07-20 11:44:25 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+*Return: void
+*/
+
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
