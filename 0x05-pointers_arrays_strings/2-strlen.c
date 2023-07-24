16cfab8084553ede48e32d56c2ca9dfa13910dd4
Amna <amnabaara287@gmail.com>
2023-07-24 16:44:22 -0700

    My first commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,6 @@
 #include <string.h>
 /**
  * _strlen - return the length of a string
- * Owned By Bwave
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
