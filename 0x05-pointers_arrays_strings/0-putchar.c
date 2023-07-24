3d8b61822ad906858ae3f743839575e88a3d5c14
Amna <amnabaara287@gmail.com>
2023-07-24 15:55:16 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
