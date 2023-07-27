3a2cdf0e41fc0f9f82423ca3d44dcbf10eab7f77
Amna <amnabaara287@gmail.com>
2023-07-27 16:26:40 -0700

    My first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,15 @@
   */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
-
-	while (dest[dlen])
-	{
-		dlen++;
-	}
-
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-
-	dest[dlen] = '\0';
-	return (dest);
+int c, c2;
+c = 0;
+while (dest[c])
+{
+c++;
+}
+for (c2 = 0; src[c2]; c2++)
+{
+dest[c++] = src[c2];
+}
+return (dest);
 }
